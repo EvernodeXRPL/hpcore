c689f99d21f3ba3288e2c465f6cb59f5e2e5c80d
Ravin Perera <33562092+ravinsp@users.noreply.github.com>
2019-09-25 03:35:04 -0400

    Added initial temp changes to generate keys using libsodium.

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,21 @@
 
 #include <cstdio>
 #include <iostream>
+#include <sodium.h>
 
 using namespace std;
 
 int main(int argc, char** argv) {
 
+    if (sodium_init() < 0) {
+        cout << "sodium_init failed.\n";
+        return 1;
+    }
+
+    unsigned char publickey[crypto_box_PUBLICKEYBYTES];
+    unsigned char privatekey[crypto_box_SECRETKEYBYTES];
+    crypto_box_keypair(publickey, privatekey);
+    
     cout << "exited normally\n";
     return 0;
 } 
