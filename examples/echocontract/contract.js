84f43b754947f7c6873cb14caff2bb4057eb5286
Ravin Perera <33562092+ravinsp@users.noreply.github.com>
2019-10-18 23:47:28 +0530

    Added Docker support.

--- examples/echocontract/contract.js
+++ examples/echocontract/contract.js
@@ -2,9 +2,8 @@ process.on('uncaughtException', (err) => {
     console.error('There was an uncaught error', err)
 })
 const fs = require('fs')
-const pipe = require('posix-pipe-fork-exec')
 
-let input = Buffer.from(pipe.getfdbytes(0)).toString()
+let input = fs.readFileSync(0, 'utf8');
 console.log("===Sample contract started===");
 console.log("Contract args received from hp: " + input);
 
@@ -12,7 +11,7 @@ let hpargs = JSON.parse(input);
 
 Object.keys(hpargs.usrfd).forEach(function (key, index) {
     let userfds = hpargs.usrfd[key];
-    let userinput = Buffer.from(pipe.getfdbytes(userfds[0])).toString().trim();
+    let userinput = fs.readFileSync(userfds[0], 'utf8');
 
     if (userinput.length > 0) {
         console.log("Input received from user " + key + ":");
@@ -21,7 +20,7 @@ Object.keys(hpargs.usrfd).forEach(function (key, index) {
     }
 });
 
-let hpinput = Buffer.from(pipe.getfdbytes(hpargs.hpfd[0])).toString().trim();
+let hpinput = fs.readFileSync(hpargs.hpfd[0], 'utf8');
 if (hpinput.length > 0) {
     console.log("Input received from hp:");
     console.log(hpinput);
