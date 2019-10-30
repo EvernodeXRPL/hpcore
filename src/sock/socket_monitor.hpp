05a7a639a6bfcd49fda48ebfd87a8141116eacad
ravidulashan <ravidu0915@gmail.com>
2019-10-30 04:34:54 -0400

    Added header definitions for socket monitor

--- src/sock/socket_monitor.hpp
+++ src/sock/socket_monitor.hpp
@@ -1,3 +1,6 @@
+#ifndef _SOCK_MONITOR_H_
+#define _SOCK_MONITOR_H_
+
 #include "../util.hpp"
 #include "socket_session.hpp"
 
@@ -6,3 +9,5 @@ namespace sock{
 template <class T>
 void threshold_monitor(util::SESSION_THRESHOLDS threshold, int64_t threshold_limit, socket_session<T> *session);
 }
+
+#endif
