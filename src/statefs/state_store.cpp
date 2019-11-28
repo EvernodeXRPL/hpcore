8fff6fde721ffb81d996ed7123acc0061bca05f9
ravinsp <33562092+ravinsp@users.noreply.github.com>
2019-11-28 19:56:49 +0530

    Added more state_store helper methods.

--- src/statefs/state_store.cpp
+++ src/statefs/state_store.cpp
@@ -1,10 +1,13 @@
 #include "../pchheader.hpp"
 #include "hasher.hpp"
 #include "state_common.hpp"
+#include "hashtree_builder.hpp"
 #include "../hplog.hpp"
 
 namespace statefs
 {
+
+// Should be replaced with flatbuffer.
 struct fs_hash_entry
 {
     bool isfile;
@@ -12,63 +15,13 @@ struct fs_hash_entry
     hasher::B2H hash;
 };
 
-struct block_hash_entry
-{
-    uint32_t blockid;
-    hasher::B2H hash;
-};
+// Map of modified/deleted files with updated blockids and hashes (if modified).
+extern std::unordered_map<std::string, std::map<uint32_t, hasher::B2H>> touchedfiles;
 
-// Map of modified/deleted files with updated blockids and hashes (if modified)
-std::unordered_map<std::string, std::map<uint32_t, hasher::B2H>> touchedfiles;
-
-int read_file_bytes(void *buf, const char *bhmapfile, const off_t start, const size_t len)
-{
-    int fd = open(bhmapfile, O_RDONLY);
-    if (fd == -1)
-    {
-        LOG_ERR << errno << "Open failed " << bhmapfile;
-        return -1;
-    }
-
-    int readbytes = pread(fd, buf, len, start);
-    close(fd);
-    if (readbytes <= 0)
-    {
-        LOG_ERR << errno << "Read failed " << bhmapfile;
-        return -1;
-    }
-
-    return readbytes;
-}
-
-int read_file_bytes(std::vector<uint8_t> &vec, const char *bhmapfile, const off_t start)
-{
-    int fd = open(bhmapfile, O_RDONLY);
-    if (fd == -1)
-    {
-        LOG_ERR << errno << "Open failed " << bhmapfile;
-        return -1;
-    }
-
-    const off_t totallen = lseek(fd, 0, SEEK_END);
-    if (totallen == -1)
-        return -1;
-
-    const size_t len = totallen - start;
-    vec.resize(len);
-
-    int readbytes = pread(fd, vec.data(), len, start);
-    close(fd);
-    if (readbytes <= 0)
-    {
-        LOG_ERR << errno << "Read failed " << bhmapfile;
-        return -1;
-    }
-    vec.resize(readbytes);
-
-    return readbytes;
-}
-
+/**
+ * Retrieves the hash list of the file system entries at a given directory.
+ * @return 0 on success. -1 on failure.
+ */
 int get_fsentry_hashes(std::vector<fs_hash_entry> &hashlist, const std::string &dirrelpath)
 {
     const std::string fullpath = current_ctx.datadir + "/" + dirrelpath;
@@ -103,16 +56,35 @@ int get_fsentry_hashes(std::vector<fs_hash_entry> &hashlist, const std::string &
     return 0;
 }
 
-int get_block_hashes(std::vector<uint8_t> &vec, const std::string &filerelpath)
+/**
+ * Retrieves the block hash map for a file.
+ * @return 0 on success. -1 on failure.
+ */
+int get_blockhashmap(std::vector<uint8_t> &vec, const std::string &filerelpath)
 {
     const std::string bhmap_path = current_ctx.hashtreedir + "/" + filerelpath + HASHMAP_EXT;
 
-    if (read_file_bytes(vec, bhmap_path.c_str(), hasher::HASH_SIZE) == -1)
+    if (read_file_bytes_toend(vec, bhmap_path.c_str(), 0) == -1)
         return -1;
 
     return 0;
 }
 
+/**
+ * Retrieves the specified data block from a state file.
+ * @return Number of bytes read on success. -1 on failure.
+ */
+int get_block(const std::string &filerelpath, const uint32_t blockid)
+{
+    std::string fullpath = current_ctx.datadir + "/" + filerelpath;
+    char buf[BLOCK_SIZE]; // Replace this with flatbuffer.
+    return read_file_bytes(buf, fullpath.c_str(), blockid * BLOCK_SIZE, BLOCK_SIZE);
+}
+
+/**
+ * Deletes the specified state sub directory and marks the change.
+ * @return 0 on success. -1 on failure.
+ */
 int delete_folder(const std::string &dirrelpath)
 {
     std::string fullpath = current_ctx.datadir + "/" + dirrelpath;
@@ -124,6 +96,10 @@ int delete_folder(const std::string &dirrelpath)
     return 0;
 }
 
+/**
+ * Deletes the specified state file and marks the change.
+ * @return 0 on success. -1 on failure.
+ */
 int delete_file(const std::string &filerelpath)
 {
     std::string fullpath = current_ctx.datadir + "/" + filerelpath;
@@ -134,6 +110,10 @@ int delete_file(const std::string &filerelpath)
     return 0;
 }
 
+/**
+ * Truncates the specified state file to the specified length and marks the change.
+ * @return 0 on success. -1 on failure.
+ */
 int truncate_file(const std::string &filerelpath, const size_t newsize)
 {
     std::string fullpath = current_ctx.datadir + "/" + filerelpath;
@@ -155,6 +135,14 @@ int truncate_file(const std::string &filerelpath, const size_t newsize)
     return 0;
 }
 
+/**
+ * Writes the specified block to a file and marks the change.
+ * @param filerelpath State data relative path of the file.
+ * @param blockid Block id to replace/write.
+ * @param buf The buffer containing data to be written.
+ * @param len Length of the buffer.
+ * @return 0 on success. -1 on failure.
+ */
 int write_block(const std::string &filerelpath, const uint32_t blockid, const void *buf, const size_t len)
 {
     std::string fullpath = current_ctx.datadir + "/" + filerelpath;
@@ -177,6 +165,86 @@ int write_block(const std::string &filerelpath, const uint32_t blockid, const vo
     hasher::B2H hash = hasher::hash(&offset, 8, buf, len);
     touchedfiles[filerelpath].emplace(blockid, hash);
     return 0;
+}
+
+/**
+ * Computes the latest hash tree with any changes recorded in touched files index.
+ * @return 0 on success. -1 on failure.
+ */
+int compute_hashtree()
+{
+    hashtree_builder htreebuilder(current_ctx);
+
+    hasher::B2H statehash = {0, 0, 0, 0};
+    int ret = htreebuilder.generate(statehash, touchedfiles);
+
+    touchedfiles.clear();
+    return ret;
+}
+
+//-----Private helper functions---------//
+
+/**
+ * Reads bytes from file into a buffer.
+ * @param buf Buffer to fill with the read bytes.
+ * @param filepath Full path to the file.
+ * @param start Starting offset to read.
+ * @param len Number of bytes to read.
+ * @return Number of bytes read on successful read. -1 on failure.
+ */
+int read_file_bytes(void *buf, const char *filepath, const off_t start, const size_t len)
+{
+    int fd = open(filepath, O_RDONLY);
+    if (fd == -1)
+    {
+        LOG_ERR << errno << "Open failed " << filepath;
+        return -1;
+    }
+
+    int readbytes = pread(fd, buf, len, start);
+    close(fd);
+    if (readbytes <= 0)
+    {
+        LOG_ERR << errno << "Read failed " << filepath;
+        return -1;
+    }
+
+    return readbytes;
+}
+
+/**
+ * Reads bytes from file into a vector. The vector size will be adjusted to the actual bytes read.
+ * @param vec Vector to fill with the read bytes.
+ * @param filepath Full path to the file.
+ * @param start Starting offset to read.
+ * @return Number of bytes read on successful read. -1 on failure.
+ */
+int read_file_bytes_toend(std::vector<uint8_t> &vec, const char *filepath, const off_t start)
+{
+    int fd = open(filepath, O_RDONLY);
+    if (fd == -1)
+    {
+        LOG_ERR << errno << "Open failed " << filepath;
+        return -1;
+    }
+
+    const off_t totallen = lseek(fd, 0, SEEK_END);
+    if (totallen == -1)
+        return -1;
+
+    const size_t len = totallen - start;
+    vec.resize(len);
+
+    int readbytes = pread(fd, vec.data(), len, start);
+    close(fd);
+    if (readbytes <= 0)
+    {
+        LOG_ERR << errno << "Read failed " << filepath;
+        return -1;
+    }
+    vec.resize(readbytes);
+
+    return readbytes;
 }
 
 } // namespace statefs
--- src/statefs/state_store.hpp
+++ src/statefs/state_store.hpp
@@ -2,23 +2,28 @@
 #define _HP_STATEFS_STATE_STORE_
 
 #include "../pchheader.hpp"
+#include "hasher.hpp"
 
 namespace statefs
 {
 
+std::unordered_map<std::string, std::map<uint32_t, hasher::B2H>> touchedfiles;
+
 //int get_fsentry_hashes(std::vector<fs_hash_entry> &hashlist, const std::string &dirrelpath);
-int get_block_hashes(std::vector<uint8_t> &vec, const std::string &filerelpath);
+int get_blockhashmap(std::vector<uint8_t> &vec, const std::string &filerelpath);
+int get_block(const std::string &filerelpath, const uint32_t blockid);
 int delete_folder(const std::string &dirrelpath);
 int delete_file(const std::string &filerelpath);
 int truncate_file(const std::string &filerelpath, const size_t newsize);
 int write_block(const std::string &filerelpath, const uint32_t blockid, const void *buf, const size_t len);
+int compute_hashtree();
 
 /**
  * Private helper functions.
  */
 
 int read_file_bytes(void *buf, const char *bhmapfile, const off_t start, const size_t len);
-int read_file_bytes(std::vector<uint8_t> &vec, const char *bhmapfile, const off_t start);
+int read_file_bytes_toend(std::vector<uint8_t> &vec, const char *bhmapfile, const off_t start);
 
 } // namespace statefs
 
