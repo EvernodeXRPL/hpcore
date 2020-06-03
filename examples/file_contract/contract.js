e58eed350254f702fac0c1d43944c5550c4d48a3
ravinsp <33562092+ravinsp@users.noreply.github.com>
2020-06-03 20:15:25 +0530

    Added example file upload contract and client.

---
+++ examples/file_contract/contract.js
@@ -0,0 +1,26 @@
+process.on('uncaughtException', (err) => {
+    console.error('There was an uncaught error', err)
+})
+const fs = require('fs')
+
+//console.log("===File contract started===");
+//console.log("Contract args received from hp: " + input);
+
+let hpargs = JSON.parse(fs.readFileSync(0, 'utf8'));
+
+// We just save execution args as an example state file change.
+fs.appendFileSync("exects.txt", "ts:" + hpargs.ts + "\n");
+
+Object.keys(hpargs.usrfd).forEach(function (key, index) {
+    let userfds = hpargs.usrfd[key];
+    let fileContent = fs.readFileSync(userfds[0]);
+
+    if (fileContent.length > 0) {
+        // Save the content into a new file.
+        var fileName = new Date().getTime().toString();
+        fs.writeFileSync(fileName, fileContent);
+        fs.writeSync(userfds[1], "Saved file (len: " + fileContent.length / 1024 + " KB)");
+    }
+});
+
+//console.log("===File contract ended===");
