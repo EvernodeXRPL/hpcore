a1852ca1a8394502c369a9cb7964008a1acc0520
ravinsp <33562092+ravinsp@users.noreply.github.com>
2020-08-04 15:52:27 +0530

    Updated nodejs contract lib.

--- examples/nodejs_contract/hp-contract-lib.js
+++ examples/nodejs_contract/hp-contract-lib.js
@@ -4,6 +4,21 @@ function HotPocketContract() {
     const hpargs = JSON.parse(fs.readFileSync(0, 'utf8'));
     this.readonly = hpargs.readonly;
     this.timestamp = hpargs.ts;
+
+    if (hpargs.lcl.length > 0) {
+        const lclParts = hpargs.lcl.split("-");
+        this.lcl = {
+            seqNo: parseInt(lclParts[0]),
+            hash: lclParts[1]
+        };
+    }
+    else { // In read-mode we will not get lcl.
+        this.lcl = {
+            seqNo: 0,
+            hash: ""
+        };
+    }
+
     this.users = {};
 
     Object.keys(hpargs.usrfd).forEach((userPubKey) => {
