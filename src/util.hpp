4df0918f8e0bcac75f91729872c3bf3bc329ee81
Ravin Perera <33562092+ravinsp@users.noreply.github.com>
2020-09-26 18:20:07 +0530

    Boost reference removal and dev setup script. (#127)
    * Removed unneeded boost library links.
    * Replaced boost string split with util function.
    * Added dev setup script.
    * Updated readme.

--- src/util.hpp
+++ src/util.hpp
@@ -108,6 +108,8 @@ namespace util
 
     int remove_file(std::string_view path);
 
+    void split_string(std::vector<std::string> &collection, std::string_view str, std::string_view delimeter);
+
 } // namespace util
 
 #endif
