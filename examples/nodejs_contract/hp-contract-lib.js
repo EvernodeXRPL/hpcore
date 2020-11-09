37e5508695c618ec7395300419dad75ef3074f19
chalith <desaman.chalith@gmail.com>
2020-11-09 10:03:19 +0530

    Adding a comment to temorary workaround

--- examples/nodejs_contract/hp-contract-lib.js
+++ examples/nodejs_contract/hp-contract-lib.js
@@ -63,6 +63,8 @@ function HotPocketContract() {
 
     this.terminate = () => {
         this.control.sendOutput("Terminated")
+        // We are still using process.kill(0) temporarily to stop contract hanging.
+        // This will be removed after the control message is implemented.
         process.kill(0);
     }
     
