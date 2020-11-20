a2883fda56e7f116ae74d924122a50eed11dd7b6
ravinsp <ravin@Ravin.geveo.local>
2020-11-20 10:46:25 +0530

    Added ts file to C echo contract.

--- examples/c_contract/echo_contract.c
+++ examples/c_contract/echo_contract.c
@@ -1,3 +1,6 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 #include "hotpocket_contract.h"
 
 // gcc echo_contract.c -o echo_contract -pthread
@@ -26,14 +29,48 @@ void echo_contract(const struct hp_contract_context *ctx)
     // Peer message sending example.
     // hp_peer_write(ctx, "Hello", 5);
 
+    if (!ctx->readonly)
+    {
+        // We just save execution timestamp as an example state file change.
+        int fd = open("exects.txt", O_RDWR | O_CREAT | O_APPEND);
+        if (fd > 0)
+        {
+            char tsbuf[20];
+            sprintf(tsbuf, "%lu\n", ctx->timestamp);
+            struct iovec vec[2] = {{"ts:", 4}, {(void *)tsbuf, 20}};
+            writev(fd, vec, 2);
+            close(fd);
+        }
+    }
+
     // Blocking call. This will block until all user messages are looped.
     hp_user_message_loop(ctx, on_user_message);
 }
 
 void on_user_message(const struct hp_contract_context *ctx, const struct hp_user *user, const void *buf, const uint32_t len)
 {
-    struct iovec vec[2] = {{"Echoing: ", 9}, {(void *)buf, len}};
-    hp_user_writev(user, vec, 2);
+    if (strncmp(buf, "ts", 2) == 0)
+    {
+        int fd = open("exects.txt", O_RDONLY);
+        if (fd > 0)
+        {
+            struct stat st;
+            if (fstat(fd, &st) != -1)
+            {
+                char tsbuf[st.st_size];
+                if (read(fd, tsbuf, st.st_size) > 0)
+                {
+                    hp_user_write(user, tsbuf, st.st_size);
+                }
+            }
+            close(fd);
+        }
+    }
+    else
+    {
+        struct iovec vec[2] = {{"Echoing: ", 9}, {(void *)buf, len}};
+        hp_user_writev(user, vec, 2);
+    }
 }
 
 // Peer message handler func.
