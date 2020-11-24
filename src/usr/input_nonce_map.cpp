9fac814df8ee56b6244d80cd9cca78cbe0f9e39d
ravinsp <ravin@Ravin.geveo.local>
2020-11-24 22:56:13 +0530

    Added input_nonce_map.

---
+++ src/usr/input_nonce_map.cpp
@@ -0,0 +1,54 @@
+#include "../pchheader.hpp"
+#include "../util/util.hpp"
+#include "input_nonce_map.hpp"
+
+namespace usr
+{
+    constexpr uint64_t TTL = 300000; // 5 minutes.
+    constexpr uint16_t CLEANUP_THRESHOLD = 256;
+
+    bool input_nonce_map::is_valid(const std::string &pubkey, const std::string &nonce)
+    {
+        bool valid = false;
+
+        const uint64_t now = util::get_epoch_milliseconds();
+        auto itr = nonce_map.find(pubkey);
+        if (itr == nonce_map.end())
+        {
+            valid = true;
+            nonce_map.emplace(pubkey, std::pair<std::string, uint64_t>(nonce, util::get_epoch_milliseconds() + TTL));
+        }
+        else
+        {
+            const std::string &existing_nonce = itr->second.first;
+            const uint64_t expire_on = itr->second.second;
+            valid = (expire_on <= now || existing_nonce < nonce);
+
+            if (valid)
+            {
+                itr->second.first = nonce;
+                itr->second.second = now + TTL;
+            }
+        }
+
+        if (nonce_map.size() > CLEANUP_THRESHOLD)
+            cleanup();
+
+        return valid;
+    }
+
+    void input_nonce_map::cleanup()
+    {
+        const uint64_t now = util::get_epoch_milliseconds();
+
+        for (auto itr = nonce_map.begin(); itr != nonce_map.end();)
+        {
+            const uint64_t expire_on = itr->second.second;
+            if (expire_on <= now)
+                itr = nonce_map.erase(itr);
+            else
+                itr++;
+        }
+    }
+
+} // namespace usr
