efbd775fa1d42cb9cdddc3d58e72c6ef8a4f1a6f
Ravin Perera <33562092+ravinsp@users.noreply.github.com>
2020-11-30 10:03:06 +0530

    Refactored C contract lib interface. (#175)

--- examples/c_contract/echo_contract.c
+++ examples/c_contract/echo_contract.c
@@ -3,52 +3,79 @@
 #include <sys/stat.h>
 #include "hotpocket_contract.h"
 
-// gcc echo_contract.c -o echo_contract -pthread
+// gcc echo_contract.c -o echo_contract
 
-void echo_contract(const struct hp_contract_context *ctx);
-void on_user_message(const struct hp_contract_context *ctx, const struct hp_user *user, const void *buf, const uint32_t len);
-void on_peer_message(const struct hp_contract_context *ctx, const char *peerPubKey, const void *buf, const uint32_t len);
+void store_timestamp(const uint64_t timestamp);
+void process_user_message(const struct hp_user *user, const void *buf, const uint32_t len);
 
 int main(int argc, char **argv)
 {
-    if (hp_init(echo_contract) == -1)
+    if (hp_init_contract() == -1)
         return 1;
 
-    return 0;
-}
+    const struct hp_contract_context *ctx = hp_get_context();
 
-/**
- * HP smart contract is defined as a function which takes HP contract context as an argument.
- * HP considers execution as complete, when this function returns.
- */
-void echo_contract(const struct hp_contract_context *ctx)
-{
-    // Non-blocking call. This will start the peer message (NPL) listener.
-    // hp_peer_message_listener(ctx, on_peer_message);
-
-    // Peer message sending example.
-    // hp_peer_write(ctx, "Hello", 5);
-
+    // We store the execution timestamp as an example state file change.
     if (!ctx->readonly)
+        store_timestamp(ctx->timestamp);
+
+    // Read and process all user inputs from the mmap.
+    const void *input_mmap = hp_init_user_input_mmap();
+
+    // Iterate through all users.
+    for (int u = 0; u < ctx->users.count; u++)
     {
-        // We just save execution timestamp as an example state file change.
-        int fd = open("exects.txt", O_RDWR | O_CREAT | O_APPEND);
-        if (fd > 0)
+        const struct hp_user *user = &ctx->users.list[u];
+
+        // Iterate through all inputs from this user.
+        for (int i = 0; i < user->inputs.count; i++)
         {
-            char tsbuf[20];
-            memset(tsbuf, 0, 20);
-            sprintf(tsbuf, "%lu\n", ctx->timestamp);
-            struct iovec vec[2] = {{"ts:", 4}, {(void *)tsbuf, 20}};
-            writev(fd, vec, 2);
-            close(fd);
+            const struct hp_user_input input = user->inputs.list[i];
+
+            // Instead of mmap, we can also read the inputs from 'ctx->users.in_fd' using file I/O.
+            // However, using mmap is recommended because user inputs already reside in memory.
+            const void *buf = input_mmap + input.offset;
+
+            process_user_message(user, buf, input.size);
         }
     }
 
-    // Blocking call. This will block until all user messages are looped.
-    hp_user_message_loop(ctx, on_user_message);
+    // Peer message send example:
+    // hp_write_peer_msg("Hello!", 6);
+
+    // Peer message receive example:
+    // // Allocate buffers for received message.
+    // char sender[HP_KEY_SIZE];
+    // char *msg = malloc(HP_PEER_MSG_MAX_SIZE);
+    // // Wait for 200ms for incoming message. We will receive our own message as well.
+    // const int len = hp_read_peer_msg(msg, sender, 200);
+    // if (len > 0)
+    //     printf("Received %.*s from %.*s", len, msg, HP_KEY_SIZE, sender);
+    // free(msg);
+
+    // Update UNL example:
+    // hp_update_unl("<64 char hex to add>", 1, "<64 char hex to remove>", 1);
+
+    hp_deinit_user_input_mmap();
+    hp_deinit_contract();
+    return 0;
 }
 
-void on_user_message(const struct hp_contract_context *ctx, const struct hp_user *user, const void *buf, const uint32_t len)
+void store_timestamp(const uint64_t timestamp)
+{
+    int fd = open("exects.txt", O_RDWR | O_CREAT | O_APPEND);
+    if (fd > 0)
+    {
+        char tsbuf[20];
+        memset(tsbuf, 0, 20);
+        sprintf(tsbuf, "%lu\n", timestamp);
+        struct iovec vec[2] = {{"ts:", 4}, {(void *)tsbuf, 20}};
+        writev(fd, vec, 2);
+        close(fd);
+    }
+}
+
+void process_user_message(const struct hp_user *user, const void *buf, const uint32_t len)
 {
     if (strncmp(buf, "ts", 2) == 0)
     {
@@ -61,7 +88,7 @@ void on_user_message(const struct hp_contract_context *ctx, const struct hp_user
                 char tsbuf[st.st_size];
                 if (read(fd, tsbuf, st.st_size) > 0)
                 {
-                    hp_user_write(user, tsbuf, st.st_size);
+                    hp_write_user_msg(user, tsbuf, st.st_size);
                 }
             }
             close(fd);
@@ -70,11 +97,6 @@ void on_user_message(const struct hp_contract_context *ctx, const struct hp_user
     else
     {
         struct iovec vec[2] = {{"Echoing: ", 9}, {(void *)buf, len}};
-        hp_user_writev(user, vec, 2);
+        hp_writev_user_msg(user, vec, 2);
     }
 }
-
-// Peer message handler func.
-// void on_peer_message(const struct hp_contract_context *ctx, const char *peerPubKey, const void *buf, const uint32_t len)
-// {
-// }
