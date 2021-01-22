70493d827df70e316f1e550e22393ebfd954f14f
Ravin Perera <33562092+ravinsp@users.noreply.github.com>
2021-01-22 16:36:34 +0530

    Contract id mismatch log message. (#227)
    * Added contract id mismatch log.
    * Added individual docker image run script.

---
+++ test/local-cluster/rundir.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Runs the specified contract directory with hpcore docker image.
+# This script assumes you already have the hpcore docker image and 'hpnet' virtual docker network.
+# Usage: ./rundir.sh 1
+
+# Validate the node count arg.
+if [ -z "$1" ]; then
+  echo "Error: Please provide contract directory to run."
+  exit 1
+else
+  echo "Starting docker container..."
+fi
+
+dir=$(realpath $1)
+dirname=$(basename  $dir)
+n=$1
+
+let pubport=8080
+
+# Mount the directory $dir into hpcore docker container and run.
+# We specify --network=hpnet so all nodes will communicate via 'hpnet' docker virtual network.
+# We specify --name for each node so it will be the virtual dns name for each node.
+docker run --rm -t -i --network=hpnet --name=hp_$dirname \
+    -p ${pubport}:${pubport} \
+    --device /dev/fuse --cap-add SYS_ADMIN --security-opt apparmor:unconfined \
+    --mount type=bind,source=$dir,target=/contract \
+    hpcore:latest run /contract
