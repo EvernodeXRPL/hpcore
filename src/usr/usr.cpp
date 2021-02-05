0852445351bd343815538c4d17ea8ad71c4412c3
ravinsp <33562092+ravinsp@users.noreply.github.com>
2021-02-05 13:08:25 +0530

    Added concurrent_read_reqeuests config.

--- src/usr/usr.cpp
+++ src/usr/usr.cpp
@@ -145,8 +145,8 @@ namespace usr
 
             if (msg_type == msg::usrmsg::MSGTYPE_CONTRACT_READ_REQUEST)
             {
-                // Ignore the request if contract execution is disabled.
-                if (!conf::cfg.contract.execute)
+                // Ignore the request if contract execution is disabled or read requests disallowed.
+                if (!conf::cfg.contract.execute || conf::cfg.user.concurrent_read_reqeuests == 0)
                     return 0;
 
                 std::string content;
