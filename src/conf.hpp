d3d30d033070c0a0b2a620d8cf331e51c562543e
ravinsp <33562092+ravinsp@users.noreply.github.com>
2021-02-14 20:16:51 +0530

    Comments.

--- src/conf.hpp
+++ src/conf.hpp
@@ -87,8 +87,8 @@ namespace conf
 
     struct round_limits_config
     {
-        size_t user_input_bytes = 0;
-        size_t user_output_bytes = 0;
+        size_t user_input_bytes = 0;  // Max contract input bytes per user per round.
+        size_t user_output_bytes = 0; // Max contract output bytes per user per round.
     };
 
     struct contract_config
