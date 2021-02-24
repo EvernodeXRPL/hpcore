363116fc2a08c44340e3e63d75ec353c4282b112
Chalith Desaman <desaman.chalith@gmail.com>
2021-02-24 09:19:36 +0530

    Added user input max ledger seq offset limit config (#256)
    - Added new config for max input ledger offset inside contract section.
    - Updated implementation of getting and updating patch config inside node and c contracts.
    - Skip inputs if max ledger offset exceeds, when handling user inputs and nup messages.

--- src/msg/usrmsg_common.hpp
+++ src/msg/usrmsg_common.hpp
@@ -56,6 +56,7 @@ namespace msg::usrmsg
     constexpr const char *REASON_BAD_SIG = "bad_sig";
     constexpr const char *REASON_APPBILL_BALANCE_EXCEEDED = "appbill_balance_exceeded";
     constexpr const char *REASON_MAX_LEDGER_EXPIRED = "max_ledger_expired";
+    constexpr const char *REASON_MAX_LEDGER_OFFSET_EXCEEDED = "max_ledger_offset_exceeded";
     constexpr const char *REASON_NONCE_EXPIRED = "nonce_expired";
     constexpr const char *REASON_ALREADY_SUBMITTED = "already_submitted";
     constexpr const char *REASON_NONCE_OVERFLOW = "nonce_overflow";
