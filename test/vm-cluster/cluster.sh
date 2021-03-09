5199b76fb26305081a5f56191cfd37c557fee3b3
Savinda Senevirathne <savindadilsara@gmail.com>
2021-03-09 09:28:39 +0530

    Fixing vm script and allowing only single sync target per vpath.

--- test/vm-cluster/cluster.sh
+++ test/vm-cluster/cluster.sh
@@ -36,9 +36,9 @@ else
 fi
 
 contconfig=$(jq -r ".contracts[] | select(.name == \"${CONTRACT}\") | .config" $conf)
-if [ "$contconfig" = "" ]; then
+if [ "$contconfig" = "" ] || [ "$contconfig" = "{}" ]; then
     # Apply default config.
-    contconfig="{user: {'port': 8080 }, mesh:{ 'port': 22860}, 'contract': {'roundtime': 2000 }, 'log':{'loglevel': 'dbg', 'loggers':['console','file']}}"
+    contconfig="{\"user\": {\"port\": 8080}, \"mesh\":{ \"port\": 22860}, \"contract\": {\"roundtime\": 2000 }, \"log\":{\"loglevel\": \"inf\", \"loggers\":[\"console\",\"file\"]}}"
 fi
 
 vmpass=$(jq -r '.vmpass' $conf)
