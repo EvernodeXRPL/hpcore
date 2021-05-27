33233a4bfafb55de0824956b1045031dbd3fc001
ravinsp <33562092+ravinsp@users.noreply.github.com>
2021-05-27 14:50:19 +0530

    Added vm group create.

--- test/vm-cluster/vultr.sh
+++ test/vm-cluster/vultr.sh
@@ -28,7 +28,7 @@ fi
 startscriptid=$(jq -c -r ".vultr.startup_script_id" $configfile)
 sshkeyids=$(jq -c -r ".vultr.ssh_key_ids" $configfile)
 
-# Common api calling functions.
+# Common api calling function. (params: httpmethod, endpoint, bodyparams)
 function apicall() {
     local url="https://api.vultr.com/v2/$2"
     local _result=""
@@ -41,7 +41,7 @@ function apicall() {
     local _parts
     readarray -t _parts <<<"$_result" # break parts by new line.
     if [[ ${_parts[1]} == 2* ]]; then # Check for 2xx status code.
-        echo ${_parts[0]}
+        [ ! -z "${_parts[0]}" ] && echo ${_parts[0]} # Return api output if there is any.
     else
         >&2 echo "Error on $1 $url code:${_parts[1]} body:${_parts[0]}"
         exit 1
@@ -80,6 +80,8 @@ function getsshkeys() {
 function getstartscripts() {
     apiget "startup-scripts"
 }
+
+# Creates a vm. (params: groupname, vmname, regionid)
 function createvm() {
     local _vminfo=$(apipost "instances" '{"tag":"'$1'", "label":"'$2'", "region":"'$3'", "os_id":'$osid', "plan":"'$planid'", "hostname":"'$2'", "script_id":"'$startscriptid'", "sshkey_id":'$sshkeyids', "backups":"disabled"}')
     [ -z "$_vminfo" ] && exit 1
@@ -95,22 +97,28 @@ function createvm() {
             break
         fi
     done
-    echo $_vmip
+    echo $2": "$_vmip
 }
+
+# Deletes a vm. (params: id)
 function deletevm() {
-    echo "Deleting vm "$1
+    echo "Deleting vm "$1"..."
     apidelete "instances" $1
 }
-function getgroupids() {
-    [ -z "$1" ] && >&2 echo "getgroupids: Group name not specified." && exit 1
+
+# Returns space-delimited list of vm ids in specified group. (params: groupname)
+function getgroupvmids() {
+    [ -z "$1" ] && >&2 echo "getgroupvmids: Group name not specified." && exit 1
     local _list=$(apigetquery "instances" "tag=$1")
     [ -z "$_list" ] && exit 1
     local _ids=$(echo $_list | jq -r ".instances | .[] | .id")
     echo $_ids
 }
-function deletegroup() {
+
+# Deletes a group of vms. (params: groupname)
+function deletevmgroup() {
     echo "Deleting all vms in group '"$1"'..."
-    local _ids=$(getgroupids "$1")
+    local _ids=$(getgroupvmids "$1")
     [ -z "$_ids" ] && exit 1
     local _arr
     readarray -d " " -t _arr <<<"$_ids" # break parts by space character.
@@ -120,8 +128,19 @@ function deletegroup() {
         deletevm $id &
     done
     wait
-    echo "Delete complete."
+    echo "Done."
+}
+function createvmgroup() {
+    echo "Creating "$2" vms in group '"$1"'..."
+    local num=""
+    for (( i=1; i<=$2; i++ ))
+    do
+        num=$(printf "%03d" $i) # Pad left with 3 zeros.
+        createvm "$1" "$1$num" "atl" &
+    done
+    wait
+    echo "Done."
 }
 
-# vmip=$(createvm "test" "testvm2" "atl")
-# deletegroup "test"
+# createvmgroup "testb" 3
+# deletevmgroup "testb"
