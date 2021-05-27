8c4dc74d73406fcd4559335f0cced63ee1162abe
ravinsp <33562092+ravinsp@users.noreply.github.com>
2021-05-27 12:38:43 +0530

    Added error handling.

--- test/vm-cluster/vultr.sh
+++ test/vm-cluster/vultr.sh
@@ -30,10 +30,21 @@ sshkeyids=$(jq -c -r ".vultr.ssh_key_ids" $configfile)
 
 # Common api calling functions.
 function apicall() {
+    local url="https://api.vultr.com/v2/$2"
+    local _result=""
     if [ -z "$3" ]; then
-        curl --silent "https://api.vultr.com/v2/$2" -X $1 -H "Authorization: Bearer $apikey" -H "Content-Type: application/json"
+        _result=$(curl --silent "$url" -X $1 -H "Authorization: Bearer $apikey" -H "Content-Type: application/json" -w "\n%{http_code}")
     else
-        curl --silent "https://api.vultr.com/v2/$2" -X $1 -H "Authorization: Bearer $apikey" -H "Content-Type: application/json" --data "$3"
+        _result=$(curl --silent "$url" -X $1 -H "Authorization: Bearer $apikey" -H "Content-Type: application/json" -w "\n%{http_code}" --data "$3")
+    fi
+    
+    local _parts
+    readarray -t _parts <<<"$_result"
+    if [ "${_parts[1]}" == "200" ]; then
+        echo ${_parts[0]}
+    else
+        >&2 echo "Error on $1 $url code:${_parts[1]} body:${_parts[0]}"
+        exit 1
     fi
 }
 function apiget() {
@@ -64,17 +75,22 @@ function getstartscripts() {
     apiget "startup-scripts"
 }
 function createvm() {
-    # vminfo=$(apipost "instances" '{"label":"'$1'", "region":"'$2'", "os_id":'$osid', "plan":"'$planid'", "hostname":"'$1'", "script_id":"'$startscriptid'", "sshkey_id":'$sshkeyids', "tag":"scriptvm", "backups":"disabled"}')
-    vminfo='{"instance":{"id":"c26e52bb-033f-4ce2-8570-2584969083d8","os":"Ubuntu 20.04 x64","ram":1024,"disk":0,"main_ip":"0.0.0.0","vcpu_count":1,"region":"atl","plan":"vc2-1c-1gb","date_created":"2021-05-27T02:16:12+00:00","status":"pending","allowed_bandwidth":1000,"netmask_v4":"","gateway_v4":"0.0.0.0","power_status":"running","server_status":"none","v6_network":"","v6_main_ip":"","v6_network_size":0,"label":"testvm1","internal_ip":"","kvm":"","tag":"scriptvm","os_id":387,"app_id":0,"firewall_group_id":"","features":[],"default_password":"*2rQS[%#BsJeK)9o"}}'
-    vmid=$(echo $vminfo | jq -r ".instance.id")
-    vmip=$(echo $vminfo | jq -r ".instance.main_ip")
-    if [ "$vmip" == "0.0.0.0" ]; then
-        sleep 0.5
-        vminfo=$(apiget "instances" $vmid)
-        vmip=$(echo $vminfo | jq -r ".instance.main_ip")
-    fi
-    echo $vmip
+    local _vminfo=$(apipost "instances" '{"tag":"'$1'", "label":"'$2'", "region":"'$3'", "os_id":'$osid', "plan":"'$planid'", "hostname":"'$2'", "script_id":"'$startscriptid'", "sshkey_id":'$sshkeyids', "backups":"disabled"}')
+    [ -z "$_vminfo" ] && exit 1
+    local _vmid=$(echo $_vminfo | jq -r ".instance.id")
+    local _vmip=$(echo $_vminfo | jq -r ".instance.main_ip")
+    for (( i=0; i<20; i++ ))
+    do
+        if [ "$_vmip" == "0.0.0.0" ]; then
+            sleep 1
+            _vminfo=$(apiget "instances" $_vmid)
+            _vmip=$(echo $_vminfo | jq -r ".instance.main_ip")
+        else
+            break
+        fi
+    done
+    echo $_vmip
 }
 
-#5cabcfea-2ec5-4b0b-bdaa-c17c15370dd9
-createvm "testvm1" "atl"
+vmip=$(createvm "test" "testvm1" "atl")
+#echo $vmip
