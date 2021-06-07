a3ad595fff0703915139ae68af820e5db1cd5dcc
ravinsp <33562092+ravinsp@users.noreply.github.com>
2021-06-07 23:27:48 +0530

    Added table storage upload.

--- test/vm-cluster/stream.js
+++ test/vm-cluster/stream.js
@@ -1,13 +1,18 @@
 const HotPocket = require('../../examples/js_client/lib/hp-client-lib');
+const azure = require('azure-storage');
 const fs = require('fs');
 
 const reconnectDelayMax = 60000;
 const dispatchInterval = 1000;
+const stateUploadInterval = 10000;
 const metricsTrackInterval = 10000;
-const dispatchBatchSize = 10;
+const eventsBatchSize = 10;
+const tableBatchSize = 20;
 
 let keys = null;
 let vultrApiKey = null;
+let azureTable = null;
+let tableSvc = null;
 const queue = [];
 const metrics = {};
 const nodes = {};
@@ -23,6 +28,14 @@ async function main() {
     const config = JSON.parse(fs.readFileSync("config.json"));
     vultrApiKey = config.vultr.api_key;
 
+    // Create Azure table service.
+    if (!config.azure_table) {
+        console.log("Azure table config missing.");
+        return;
+    }
+    tableSvc = azure.createTableServiceWithSas(config.azure_table.host, config.azure_table.sas);
+    azureTable = config.azure_table.table;
+
     // We only consider clusters with stream=true.
     const clusters = config.contracts.filter(c => c.stream == true).map(c => ({
         name: c.name,
@@ -34,7 +47,12 @@ async function main() {
     if (clusters.length == 0)
         return;
 
+    // Start node state uploader.
+    // This keeps uploading latest node state into table storage.
+    nodeStateUploader();
+
     // Start event dispatcher.
+    // This keeps sending events to the ingestion endpoint.
     eventDispatcher();
 
     // Start event metrics tracker.
@@ -49,14 +67,47 @@ function eventDispatcher() {
     // Dispatch all queued events in batches.
     const events = queue.splice(0);
 
-    var i, j, batch;
-    for (i = 0, j = events.length; i < j; i += dispatchBatchSize) {
-        batch = events.slice(i, i + dispatchBatchSize);
+    for (let i = 0, j = events.length; i < j; i += eventsBatchSize) {
+        const batch = events.slice(i, i + eventsBatchSize);
 
         // TODO: Upload batch of events.
     }
 
     setTimeout(() => eventDispatcher(), dispatchInterval);
+}
+
+function nodeStateUploader() {
+
+    const uploads = [];
+
+    for (const [uri, node] of Object.entries(nodes)) {
+        const ev = node.lastEvent;
+        if (ev && !ev.uploaded) {
+            ev.uploaded = true;
+            uploads.push(node);
+        }
+    }
+
+    const ent = azure.TableUtilities.entityGenerator;
+    for (let i = 0, j = uploads.length; i < j; i += tableBatchSize) {
+        const batch = uploads.slice(i, i + tableBatchSize);
+
+        const tableBatch = new azure.TableBatch();
+
+        for (const node of batch) {
+            const ev = node.lastEvent;
+            tableBatch.insertOrReplaceEntity({
+                PartitionKey: ent.String(node.cluster),
+                RowKey: ent.String(node.idx.toString()),
+                uri: ent.String(node.uri),
+                data: ent.String(JSON.stringify({ timestamp: ev.timestamp, data: ev.data }))
+            });
+        }
+
+        tableSvc.executeBatch(azureTable, tableBatch, (err) => err && console.log(err));
+    }
+
+    setTimeout(() => nodeStateUploader(), stateUploadInterval);
 }
 
 function metricsTracker() {
@@ -77,40 +128,45 @@ async function streamCluster(cluster) {
 }
 
 function streamNode(clusterName, nodeIdx, host, port) {
-    const serverUri = `wss://${host}:${port}`;
-    nodes[serverUri] = {
-        failureCount: 0
+    const uri = `wss://${host}:${port}`;
+    const node = {
+        cluster: clusterName,
+        idx: nodeIdx,
+        uri: uri,
+        failureCount: 0,
+        lastEvent: null
     };
 
-    establishClientConnection(clusterName, nodeIdx, serverUri);
+    nodes[uri] = node;
+
+    establishClientConnection(node);
 }
 
-async function establishClientConnection(clusterName, idx, serverUri) {
+async function establishClientConnection(node) {
 
-    const hpc = await HotPocket.createClient([serverUri], keys, { connectionTimeoutMs: 2000 });
+    const hpc = await HotPocket.createClient([node.uri], keys, { connectionTimeoutMs: 2000 });
 
     hpc.on(HotPocket.events.disconnect, () => {
-        onConnectionFail(clusterName, idx, serverUri);
+        onConnectionFail(node);
     });
 
     // This will get fired when any ledger event occurs (ledger created, sync status change).
     hpc.on(HotPocket.events.ledgerEvent, (ev) => {
-        reportEvent(clusterName, idx, serverUri, ev);
+        reportEvent(node, ev);
     });
 
     // Establish HotPocket connection.
     if (!await hpc.connect()) {
-        onConnectionFail(clusterName, idx, serverUri);
+        onConnectionFail(node);
     }
     else {
-        nodes[serverUri].failureCount = 0;
+        node.failureCount = 0;
         await hpc.subscribe(HotPocket.notificationChannels.ledgerEvent);
     }
 }
 
-function onConnectionFail(clusterName, idx, serverUri) {
+function onConnectionFail(node) {
 
-    const node = nodes[serverUri];
     node.failureCount++;
 
     // Calculate back-off delay.
@@ -118,26 +174,32 @@ function onConnectionFail(clusterName, idx, serverUri) {
     if (delay > reconnectDelayMax)
         delay = reconnectDelayMax;
 
-    console.log(`${serverUri} connection failed. Backoff ${delay}ms.`);
+    console.log(`${node.uri} connection failed. Backoff ${delay}ms.`);
 
     // Report offline event and connect again after a small delay.
-    reportEvent(clusterName, idx, serverUri, { event: "offline" });
-    setTimeout(() => establishClientConnection(clusterName, idx, serverUri), delay);
+    reportEvent(node, { event: "offline" });
+    setTimeout(() => establishClientConnection(node), delay);
 }
 
-async function reportEvent(clusterName, nodeIdx, serverUri, ev) {
-    const obj = {
-        cluster: clusterName,
-        idx: nodeIdx,
-        uri: serverUri,
-        timestamp: new Date().getTime(), // Epoch milliseconds.
+async function reportEvent(node, ev) {
+
+    const ts = new Date().getTime(); // Epoch milliseconds.
+
+    queue.push({
+        cluster: node.cluster,
+        idx: node.idx,
+        uri: node.uri,
+        timestamp: ts,
         data: ev
-    }
+    });
 
-    queue.push(obj);
+    nodes[node.uri].lastEvent = {
+        timestamp: ts,
+        data: ev
+    };
 
-    const count = metrics[clusterName];
-    metrics[clusterName] = count ? (count + 1) : 1;
+    const count = metrics[node.cluster];
+    metrics[node.cluster] = count ? (count + 1) : 1;
 }
 
 main();
