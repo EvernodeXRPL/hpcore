e37cbd84347b5b1ef7073b213fb265203b236b18
ravinsp <33562092+ravinsp@users.noreply.github.com>
2021-06-12 16:00:56 +0530

    Update2.

--- test/docker/build.sh
+++ test/docker/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 hpcoredir=$(realpath ../..)
-docker build -t hpcore:focal -f ./Dockerfile.hp-ubuntu-focal $hpcoredir
+
+mkdir -p bin
+cp $hpcoredir/build/hpcore $hpcoredir/test/bin/{hpfs,hpws,libfuse3.so.3,libblake3.so,fusermount3} ./bin/
+strip ./bin/hpcore
+docker build -t hpcore:focal -f ./Dockerfile.hp-ubuntu-focal ./bin
+rm -r bin
