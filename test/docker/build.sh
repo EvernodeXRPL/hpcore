df647cf47e3ae3bf5f2f6a2da9c9f245d645ae8d
ravinsp <33562092+ravinsp@users.noreply.github.com>
2021-06-12 21:09:52 +0530

    Images.

--- test/docker/build.sh
+++ test/docker/build.sh
@@ -2,8 +2,12 @@
 
 hpcoredir=$(realpath ../..)
 
+# Build base Ubuntu image.
 mkdir -p bin
 cp $hpcoredir/build/hpcore $hpcoredir/test/bin/{hpfs,hpws,libfuse3.so.3,libblake3.so,fusermount3} ./bin/
 strip ./bin/hpcore
-docker build -t hpcore:focal -f ./Dockerfile.hp-ubuntu-focal ./bin
+docker build -t hotpocket:ubt.20.04 -f ./Dockerfile.ubt.20.04 ./bin
 rm -r bin
+
+# NodeJs image.
+docker build -t hotpocket:ubt.20.04-njs.14 -f ./Dockerfile.ubt.20.04-njs.14 .
