38c35a4d9dc7eef488858f16df73b78220d9b59d
ravinsp <33562092+ravinsp@users.noreply.github.com>
2021-06-22 15:26:13 +0530

    Docker push.

--- test/docker/build.sh
+++ test/docker/build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 hpcoredir=$(realpath ../..)
+repo=hotpocketdev/hotpocket
 
 # Build base Ubuntu image.
 mkdir -p bin
 cp $hpcoredir/build/hpcore $hpcoredir/test/bin/{hpfs,hpws,libfuse3.so.3,libblake3.so,fusermount3} ./bin/
 strip ./bin/hpcore
-docker build -t hotpocket:ubt.20.04 -f ./Dockerfile.ubt.20.04 ./bin
+docker build -t $repo:ubt.20.04 -f ./Dockerfile.ubt.20.04 ./bin
 rm -r bin
 
 # NodeJs image.
-docker build -t hotpocket:ubt.20.04-njs.14 -f ./Dockerfile.ubt.20.04-njs.14 .
+docker build -t $repo:ubt.20.04-njs.14 -f ./Dockerfile.ubt.20.04-njs.14 .
