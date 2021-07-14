8835de624af75fc722b25080b9f17b85d59cd8de
ravinsp <33562092+ravinsp@users.noreply.github.com>
2021-07-14 12:10:20 +0530

    Focal.

--- test/docker/build.sh
+++ test/docker/build.sh
@@ -8,7 +8,7 @@ mkdir -p bin
 cp $hpcoredir/build/{hpcore,appbill} $hpcoredir/test/bin/{hpfs,hpws,libblake3.so} ./bin/
 strip ./bin/hpcore
 
-docker build -t $repo:buster -f ./Dockerfile.buster ./bin
+docker build -t $repo:ubt.20.04 -f ./Dockerfile.ubt.20.04 ./bin
 docker build -t $repo:njs.14 -f ./Dockerfile.njs.14 ./bin
 
 rm -r bin
--- test/docker/push.sh
+++ test/docker/push.sh
@@ -3,5 +3,5 @@
 repo=hotpocketdev/hotpocket
 
 ./build.sh
-docker push $repo:buster
+docker push $repo:ubt.20.04
 docker push $repo:njs.14
