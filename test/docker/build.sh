050b6f5bd58705fb765d3793c5c6c5ff9dbe8ee8
Ravin Perera <33562092+ravinsp@users.noreply.github.com>
2022-03-08 21:12:26 +0530

    Upgraded nodejs dependencies to 16.x (#364)

--- test/docker/build.sh
+++ test/docker/build.sh
@@ -22,11 +22,11 @@ rm -r $tmp/*
 
 # NodeJs image
 # Download and place the nodejs binary in the build context.
-curl -fsSL -o $tmp/nodejs.tar.xz https://nodejs.org/dist/v14.17.3/node-v14.17.3-linux-x64.tar.xz
-tar -xvJf $tmp/nodejs.tar.xz --strip-components=2 -C $tmp/ node-v14.17.3-linux-x64/bin/node
+curl -fsSL -o $tmp/nodejs.tar.xz https://nodejs.org/dist/v16.14.0/node-v16.14.0-linux-x64.tar.xz
+tar -xvJf $tmp/nodejs.tar.xz --strip-components=2 -C $tmp/ node-v16.14.0-linux-x64/bin/node
 rm $tmp/nodejs.tar.xz
 cp ./$njsfile $tmp/
 sed -i "s/%ver%/$imgversion/g" $tmp/$njsfile
-docker build -t $img:$imgversion-ubt.20.04-njs.14 -f $tmp/$njsfile $tmp
+docker build -t $img:$imgversion-ubt.20.04-njs.16 -f $tmp/$njsfile $tmp
 
 rm -r $tmp
