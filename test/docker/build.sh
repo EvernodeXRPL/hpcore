c3052ca60d6a4d81417812edb28dfd3ea23f8842
Udith-Gayan <udith.indrakantha@gmail.com>
2022-11-16 13:06:24 +0530

    Hpversion formatted.

--- test/docker/build.sh
+++ test/docker/build.sh
@@ -13,11 +13,11 @@ strip $tmp/hpcore
 
 # Remove the revision component from hp version to make up the image version.
 hpversion=$($tmp/hpcore version | head -n 1)
-imgversion=$(echo "${hpversion}")
+imgversion=$(echo "${hpversion% }")
 # imgversion="latest"
 
 # Ubuntu base image
-docker build -t $img:$imgversion-ubt.20.04 -t $img:latest-ubt.20.04 -f ./$basefile $tmp/
+docker build -t "$img:$imgversion-ubt.20.04" -t "$img:latest-ubt.20.04" -f ./$basefile $tmp/
 rm -r $tmp/*
 
 # NodeJs image
@@ -27,6 +27,6 @@ tar -xvJf $tmp/nodejs.tar.xz --strip-components=2 -C $tmp/ node-v16.14.0-linux-x
 rm $tmp/nodejs.tar.xz
 cp ./$njsfile $tmp/
 sed -i "s/%ver%/$imgversion/g" $tmp/$njsfile
-docker build -t $img:$imgversion-ubt.20.04-njs.16 -t $img:latest-ubt.20.04-njs.16 -f $tmp/$njsfile $tmp
+docker build -t "$img:$imgversion-ubt.20.04-njs.16" -t "$img:latest-ubt.20.04-njs.16" -f $tmp/$njsfile $tmp
 
 rm -r $tmp
